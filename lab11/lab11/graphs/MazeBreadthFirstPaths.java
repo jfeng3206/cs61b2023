65b1c8bd1ffa0163aba238b9171cdb30b0fe795b
auchmargust <fengjingya320@gmail.com>
2024-01-10 19:26:37 -0800

    finish lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,4 +1,6 @@
 package lab11.graphs;
+import edu.princeton.cs.algs4.Queue;
+
 
 /**
  *  @author Josh Hug
@@ -9,21 +11,53 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze =m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[s]=true;
+
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(s);
+        while(!q.isEmpty()){
+            int source = q.dequeue();
+            for(int neighbour: maze.adj(source)){
+                if(!marked[neighbour]){
+                    marked[neighbour]=true;
+                    q.enqueue(neighbour);
+                    edgeTo[neighbour]=source;
+                    distTo[neighbour]=distTo[source]+1;
+                    announce();
+
+                    if(neighbour==t){
+                        targetFound=true;
+                    }
+
+                    if(targetFound) return;
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+         bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeDepthFirstPaths.java
+++ lab11/lab11/graphs/MazeDepthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+
+
 /**
  *  @author Josh Hug
  */
